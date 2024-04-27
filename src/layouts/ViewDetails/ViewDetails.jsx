b368caac44c16f6a6f2f2fbfe3c0d450360f2210
mahfuj926 <mahfujrahman.jewel@gmail.com>
2024-04-27 23:36:28 +0600

    Added view details page

--- src/layouts/ViewDetails/ViewDetails.jsx
+++ src/layouts/ViewDetails/ViewDetails.jsx
@@ -4,27 +4,29 @@ import { useLoaderData } from "react-router-dom";
 const ViewDetails = () => {
 
     const spot = useLoaderData();
-    const { img, spotName, countryName, location, description, cost,seasonality, travelTime,  visitor } = spot || {};
+    const { img, spotName, countryName, location, description, cost, seasonality, travelTime, visitor } = spot || {};
     return (
-        <div className="card w-96 bg-base-100 mt-10 border-2 border-gray-300">
-        <figure className="px-10 pt-10">
-            <img src={img} alt="Shoes" className="rounded-xl" />
-        </figure>
-        <div className="card-body">
-            <h2 className="card-title">{spotName}</h2>
-            <p className='text-gray-600'>{description}</p>
-            <p className='border-dotted border-b-2 border-gray-300 mb-4'></p>
+        <div className="mx-auto container rounded-2xl mt-10 lg:border-2 lg:border-gray-300">
+            <div className="hero ">
+                <div className="hero-content flex-col lg:flex-row gap-20">
+                    <img src={img} className="max-w-sm h-auto rounded-lg " />
+                    <div>
+                        <h1 className="text-5xl font-bold">{spotName}</h1>
+                        <p className="py-6">{description}</p>
+                        <p className='border-dotted border-b-2 border-gray-300 mb-4'></p>
 
-            <p><span className='text-xl font-medium'>Country:</span> <span className='text-gray-600'>{countryName}</span></p>
-            <p><span className='text-xl font-medium'>Location:</span> <span className='text-gray-600'>{location}</span></p>
-            <p className='border-dotted border-b-2 border-gray-300  mb-4'></p>
-            <p><span className='text-xl font-medium'>Seasonality:</span> <span className='text-gray-600'>{seasonality}</span></p>
-            <p><span className='text-xl font-medium'>Travel Time:</span> <span className='text-gray-600'>{travelTime}</span></p>
-            <p className='border-dotted border-b-2 border-gray-300  mb-4'></p>
-            <p><span className='text-xl font-medium'>Visitor:</span> <span className='text-gray-600'>{visitor}</span></p>
-            <p><span className='text-xl font-medium'>Cost:</span> <span className='text-gray-600'>{cost}</span></p>
+                        <p><span className='text-xl font-medium'>Country:</span> <span className='text-gray-600'>{countryName}</span></p>
+                        <p><span className='text-xl font-medium'>Location:</span> <span className='text-gray-600'>{location}</span></p>
+                        <p className='border-dotted border-b-2 border-gray-300 my-4'></p>
+                        <p><span className='text-xl font-medium'>Seasonality:</span> <span className='text-gray-600'>{seasonality}</span></p>
+                        <p><span className='text-xl font-medium'>Travel time:</span> <span className='text-gray-600'>{travelTime}</span></p>
+                        <p className='border-dotted border-b-2 border-gray-300 my-4'></p>
+                        <p><span className='text-xl font-medium'>Visitor:</span> <span className='text-gray-600'>{visitor}</span></p>
+                        <p><span className='text-xl font-medium'>Cost:</span> <span className='text-gray-600'>{cost}</span></p>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
     );
 };
 
